a8c96455437626d7489c33bbb8f3f4516f3acc64
AyumuNekozuki <ayumu@nekozuki.me>
2022-10-12 22:08:43 +0900

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7271400990150352" crossOrigin="anonymous"></script>
-          <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+          <link rel="icon" type="image/x-icon" href="/favicon.png?v=20221012"></link>
 					<link rel="apple-touch-icon" sizes="180x180" href="/favicon_package/apple-touch-icon.png?v=20221012"></link>
 					<link rel="icon" type="image/png" sizes="32x32" href="/favicon_package/favicon-32x32.png?v=20221012"></link>
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon_package/favicon-16x16.png?v=20221012"></link>
