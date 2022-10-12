7e9320ac7e37983294656dfe2cfc90364e53ce9d
AyumuNekozuki <ayumu@nekozuki.me>
2022-10-12 22:45:42 +0900

    fix navToggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,47 +2,42 @@ import Link from 'next/link';
 
 export default function Header() {
 
-  if(typeof window !== 'undefined'){
-    const dom_nav_toggle: HTMLElement | null = document.getElementById('nav-toggle');
-    const dom_nav_content: HTMLElement | null = document.getElementById('nav-content');
-
-    if(dom_nav_toggle && dom_nav_content) {
-      dom_nav_toggle.onclick = function () {
-        dom_nav_content.classList.toggle("hidden");
-      }
-    }
-  }
+	const navMenuToggle = () =>{
+		console.log('navMenuToggle');
+		const navContents:HTMLElement | null = document.getElementById('nav-content')
+		navContents && navContents.classList.toggle("hidden");
+	}
 
   return (
-    <header className='flex bg-themepurple w-full drop-shadow-md px-6'>
-      <nav className='flex items-center justify-between flex-wrap bg-themepurple py-2 w-full z-10 top-0 mx-auto max-w-screen-lg'>
-        <div className='flex items-center flex-shrink-0 text-white mr-6'>
+    <header className='flex w-full px-6 bg-themepurple drop-shadow-md'>
+      <nav className='top-0 z-10 flex flex-wrap items-center justify-between w-full max-w-screen-lg py-2 mx-auto bg-themepurple'>
+        <div className='flex items-center flex-shrink-0 mr-6 text-white'>
           <h1>
             <Link className='text-white no-underline' href='/'>
-              <img className='cursor-pointer max-h-6 hover:opacity-80 transition-all' src="/img/nekolog_dark_logo.png" alt="ねころぐ" />
+              <img className='transition-all cursor-pointer max-h-6 hover:opacity-80' src="/img/nekolog_dark_logo.png" alt="ねころぐ" />
             </Link>
           </h1>
         </div>
 
         <div className='block lg:hidden'>
-          <button id='nav-toggle' className='flex items-center px-3 py-2 border rounded transition-all text-white border-white hover:bg-white hover:text-themepurple'>
-            <svg className='fill-current h-3 w-3' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><title>Menu</title><path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' /></svg>
+          <button id='nav-toggle' className='flex items-center px-3 py-2 text-white transition-all border border-white rounded hover:bg-white hover:text-themepurple' onClick={navMenuToggle}>
+            <svg className='w-3 h-3 fill-current' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><title>Menu</title><path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' /></svg>
           </button>
         </div>
 
-        <div className='w-full flex-grow lg:flex lg:items-center lg:w-auto hidden pt-3 lg:pt-0 transition-all' id='nav-content'>
-          <ul className='list-reset lg:flex justify-end flex-1 items-center'>
+        <div className='flex-grow hidden w-full pt-3 transition-all lg:flex lg:items-center lg:w-auto lg:pt-0' id='nav-content'>
+          <ul className='items-center justify-end flex-1 list-reset lg:flex'>
             <li className='mr-3'>
-              <Link href='/archive'><a className='font-mplus font-medium inline-block py-1 px-2 transition-all text-white no-underline hover:opacity-80'>Archive</a></Link>
+              <Link href='/archive'><a className='inline-block px-2 py-1 font-medium text-white no-underline transition-all font-mplus hover:opacity-80'>Archive</a></Link>
             </li>
             <li className='mr-3'>
-              <Link href='/category'><a className='font-mplus font-medium inline-block py-1 px-2 transition-all text-white no-underline hover:opacity-80'>Category</a></Link>
+              <Link href='/category'><a className='inline-block px-2 py-1 font-medium text-white no-underline transition-all font-mplus hover:opacity-80'>Category</a></Link>
             </li>
             <li className='mr-3'>
-              <Link href='/tag'><a className='font-mplus font-medium inline-block py-1 px-2 transition-all text-white no-underline hover:opacity-80'>Tag</a></Link>
+              <Link href='/tag'><a className='inline-block px-2 py-1 font-medium text-white no-underline transition-all font-mplus hover:opacity-80'>Tag</a></Link>
             </li>
             <li className='mr-3'>
-              <a className='font-mplus font-medium inline-block py-1 px-2 transition-all text-white no-underline hover:opacity-80' href='https://www.nekozuki.me/about'>About</a>
+              <a className='inline-block px-2 py-1 font-medium text-white no-underline transition-all font-mplus hover:opacity-80' href='https://www.nekozuki.me/about'>About</a>
             </li>
           </ul>
         </div>
