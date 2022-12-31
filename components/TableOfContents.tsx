51324fcb49caf7652a8c3ef0b2772dff14de3b77
AyumuNekozuki <ayumu@nekozuki.me>
2022-12-31 19:19:44 +0900

    fix mokuji

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 
 export default function TableOfContents({ toc }: any) {
-	console.log(toc)
-
   return (
     <div id='toc' className="w-full mb-3 bg-white rounded-lg shadow-card md:mx-auto">
       <div className="flex flex-col items-start p-3">
--- libs/render-toc.js
+++ libs/render-toc.js
@@ -2,7 +2,7 @@ import * as cheerio from 'cheerio';
 
 export const renderToc = (body) => {
   const $ = cheerio.load(body);
-  const headings = $('h1, h2, h3').toArray();
+  const headings = $('h1, h2, h3, h4, h5, h6').toArray();
   const toc = headings.map((data) => ({
     text: data.children[0].data,
     id: data.attribs.id,
