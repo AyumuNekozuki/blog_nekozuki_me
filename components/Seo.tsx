f36496fd6f3aae678dc74937d616e7bcee06f5d0
AyumuNekozuki <ayumu@nekozuki.me>
2023-01-25 17:18:04 +0900

    set max-length ogp-description

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,55 +1,50 @@
-import { VFC } from 'react'
-import Head from 'next/head'
+import { VFC } from 'react';
+import Head from 'next/head';
 
 interface MetaData {
-  pageTitle?: string
-  pageDescription?: string
-  pagePath?: string
-  pageImg?: string
-  pageImgWidth?: number
-  pageImgHeight?: number
-  pageType?: string
+	pageTitle?: string;
+	pageDescription?: string;
+	pagePath?: string;
+	pageImg?: string;
+	pageImgWidth?: number;
+	pageImgHeight?: number;
+	pageType?: string;
 }
 
-const Seo: VFC<MetaData> = ({
-  pageTitle,
-  pageDescription,
-  pagePath,
-  pageImg,
-  pageImgWidth,
-  pageImgHeight,
-  pageType
-}) => {
-  const defaultTitle = 'ねころぐ'
-  const defaultDescription = 'ねこづきあゆむの過去ログ倉庫です。'
-  const defaultPath = 'https://blog.nekozuki.me'
+const defaultContents = {
+	siteName: 'ねころぐ',
+	defaultTitle: 'ねころぐ',
+	defaultDescription: 'ねこづきあゆむの個人ブログです。',
+	defaultPath: 'https://blog.nekozuki.me',
+};
 
-  const title = pageTitle ? `${pageTitle} | ${defaultTitle}` : defaultTitle
-  const description = pageDescription ? pageDescription : defaultDescription
-  const url = pagePath ? `${defaultPath}${pagePath}` : defaultPath
-  const imgUrl = pageImg ? pageImg : 'https://blog.nekozuki.me/ogp.png'
-  const imgWidth = pageImgWidth ? pageImgWidth : 1280
-  const imgHeight = pageImgHeight ? pageImgHeight : 640
-  const type = pageType ? pageType : 'blog';
+const Seo: VFC<MetaData> = ({ pageTitle, pageDescription, pagePath, pageImg, pageImgWidth, pageImgHeight, pageType }) => {
+	const title = pageTitle ? `${pageTitle} | ${defaultContents.siteName}` : defaultContents.defaultTitle;
+	const description = pageDescription ? `${pageDescription?.substring(0, 20)}…` : defaultContents.defaultDescription;
+	const url = pagePath ? `${defaultContents.defaultPath}${pagePath}` : defaultContents.defaultPath;
+	const imgUrl = pageImg ? pageImg : 'https://blog.nekozuki.me/ogp.png';
+	const imgWidth = pageImgWidth ? pageImgWidth : 1280;
+	const imgHeight = pageImgHeight ? pageImgHeight : 640;
+	const type = pageType ? pageType : 'blog';
 
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <meta name="description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:site_name" content='ねころぐ' />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content={type} />
-      <meta property="og:image" content={imgUrl} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@nekozuki_2525" />
-      <meta name="twitter:creator" content="@nekozuki_2525" />
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta name='viewport' content='width=device-width,initial-scale=1.0' />
+			<meta name='description' content={description} />
+			<meta property='og:url' content={url} />
+			<meta property='og:title' content={title} />
+			<meta property='og:site_name' content={defaultContents.siteName} />
+			<meta property='og:description' content={description} />
+			<meta property='og:type' content={type} />
+			<meta property='og:image' content={imgUrl} />
+			<meta name='twitter:card' content='summary_large_image' />
+			<meta name='twitter:site' content='@nekozuki_2525' />
+			<meta name='twitter:creator' content='@nekozuki_2525' />
 
-      <link rel="canonical" href={url} />
-    </Head>
-  )
-}
+			<link rel='canonical' href={url} />
+		</Head>
+	);
+};
 
-export default Seo
+export default Seo;
