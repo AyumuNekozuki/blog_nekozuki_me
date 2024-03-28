808e9cd38b96af057fd5a46c159cecee2e367211
AyumuNekozuki <ayumu@nekozuki.me>
2024-03-28 17:18:23 +0900

    fix hydtration error

--- components/DateTimeObj.tsx
+++ components/DateTimeObj.tsx
@@ -1,6 +1,7 @@
+import dynamic from 'next/dynamic';
 import { parseISO, format } from 'date-fns';
 import ja from 'date-fns/locale/ja';
 
 export default function DateTimeObj({ dateString }: any) {
-	return <time dateTime={dateString}>{format(parseISO(dateString), 'yyyy/MM/dd HH:mm', { locale: ja })}</time>;
+	return <time suppressHydrationWarning={true} dateTime={dateString}>{format(parseISO(dateString), 'yyyy/MM/dd HH:mm', { locale: ja })}</time>;
 }
